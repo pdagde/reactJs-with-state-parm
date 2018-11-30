31f84ec07ed4ba0616502e32ee09b72038578724
pdagde <pravin.dagde@knorex.com>
2018-11-30 15:32:09 +0530

    conditional oparetor in container function

--- src/home.js
+++ src/home.js
@@ -2,13 +2,13 @@ import React from 'react';
 
     const home = ({homes}) => {
         const Homepage =homes.map(homeOne =>{
-            return (
-                <div className="home" key={homeOne.id}>
+            return homeOne.age > 24 ? (
+                 <div className="home" key={homeOne.id}>
                  <div> Name: {homeOne.name}</div>
                  <div> Age: {homeOne.age}</div>
                  <div> Belt: {homeOne.belt}</div>
                 </div>
-            )
+            ) : null
         }) 
         return (
             <div className="home">
